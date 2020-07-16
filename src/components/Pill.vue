0ed94247d39b6f916814b934944ed05534faa61e
Krizza Bullecer <krizzabullecer@gmail.com>
2020-07-17 02:08:42 +0800

    complete pill

---
+++ src/components/Pill.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pill">Full time</div>
+</template>
+
+<style lang="scss" scoped>
+.pill {
+  padding: 4px 8px;
+  width: max-content;
+  background: transparent;
+  color: $primary;
+  font-size: 12px;
+  font-weight: 700;
+  border: 1px solid $primary;
+  border-radius: 4px;
+}
+</style>
