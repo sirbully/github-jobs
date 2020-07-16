20243c51fa4cca0c2ca2a362a4b8482c3b7ad735
Krizza Bullecer <krizzabullecer@gmail.com>
2020-07-17 00:52:44 +0800

    add vue composition

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +8,8 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionAPI);
 
 new Vue({
   router,
