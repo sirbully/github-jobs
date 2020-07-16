d4d1d042d125d8d508fa030e05def36fc71d67f7
Krizza Bullecer <krizzabullecer@gmail.com>
2020-07-17 00:10:19 +0800

    complete app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,76 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <mdb-container id="app">
+    <h1 class="nav-brand">
+      Github
+      <span class="normal">Jobs</span>
+    </h1>
+    <router-view />
+  </mdb-container>
 </template>
 
+<script>
+import { mdbContainer } from 'mdbvue';
+
+export default {
+  name: 'App',
+  components: {
+    mdbContainer,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&family=Roboto:wght@400;500;700&display=swap");
+
+body {
+  font-size: 0.875rem;
+  color: $primary;
+  font-family: "Roboto", sans-serif;
 }
 
-#nav {
-  padding: 30px;
+h1,
+h6 {
+  font-family: "Poppins", sans-serif;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+h1 {
+  color: $dark;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+h6 {
+  color: $secondary;
+  font-size: 14px;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+
+i {
+  font-size: 1.25rem !important;
+}
+
+.btn {
+  font-family: "Roboto", sans-serif;
+}
+
+.nav-brand {
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin: 2rem 0;
+
+  .normal {
+    font-weight: 300;
+  }
+}
+
+// COLORS
+.color-info {
+  background-color: $info !important;
+  color: #fff !important;
+
+  .alt {
+    background-color: transparent !important;
+    border: 1px solid $info !important;
+    color: $info !important;
   }
 }
 </style>
