ab1daa0a3142051d078e36490c75420892eea0b5
Krizza Bullecer <krizzabullecer@gmail.com>
2020-07-16 22:26:39 +0800

    add mdbvue bootstrap and fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'mdbvue/lib/mdbvue.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 Vue.config.productionTip = false;
 
