5bd69f7f0292efe4bb02a5b361bfb4277aa6ed12
Krizza Bullecer <krizzabullecer@gmail.com>
2020-07-17 04:53:20 +0800

    remove vuex

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import VueCompositionAPI from '@vue/composition-api';
 import Paginate from 'vuejs-paginate';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import 'mdbvue/lib/mdbvue.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -15,6 +14,5 @@ Vue.component('paginate', Paginate);
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#app');
